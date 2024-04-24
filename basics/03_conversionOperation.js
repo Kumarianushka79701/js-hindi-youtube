d35af87efbb012ba11bc93252e9949a2003d303f
Anurai119 <142922260+Anurai119@users.noreply.github.com>
2024-04-24 23:47:18 +0530

    conversion

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score ="true"
+  console.log(typeof score);
+  console.log(typeof(score));
+
+   let valueInNumber=Number(score)
+  console.log(typeof valueInNumber);
+  console.log(valueInNumber);
+
+  // "33"=> 33
+  //"33aa"=> NaN(numaber)
+  // true=1,false=0
+  let isLoggedIn=1
+  let booleanIsLoggedIn=Boolean(isLoggedIn)
+  console.log(booleanIsLoggedIn);
+
+  //1=true; 0=false;
+  //""=false
+  // "anu" = true
+  let someNumber=22
+  let stringNumber=String(someNumber)
+  console.log(stringNumber);
+  console.log(typeof stringNumber);
+
+  
