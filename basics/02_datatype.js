95ffd65f24360bb014326487dd5fc69016ae50f1
Anurai119 <142922260+Anurai119@users.noreply.github.com>
2024-04-24 23:30:28 +0530

    first chages

---
+++ basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict"; // trate all js code as a newer version 
+// alert("hello") // wea are using node js not brauser
+// number => 2 
+// bigint
+// string-"",''
+// boolean=true
+// null 
+// undefined=> 
+// symbol => unique
+
+
+
+// object
+console.log(typeof "anushka");
+console.log(typeof undefined);
+
+
+// diff between null and undefined => null is a object and undefined is a data type 
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,19 @@
 console.log("anushka")
+const accountId =4545
+let accountEmail="anushka160599@gmail.com"
+var accountPassword = "12345"
+accountCity="chhapra"
+let accountState;
+
+// accountId=2  // not allowed to change
+accountEmail="anu122@gmail.com"
+accountPassword="4321"
+accountCity="noida"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+console.log(accountId);
+console.log(accountEmail);
+
+console.log(accountPassword);
+
+console.log(accountCity);
+
